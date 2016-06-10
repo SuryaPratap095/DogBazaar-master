b7cd0f12a4722277ecb1ee95510fd0737b435e5d
Surya Solanki <surya.solanki@nihilent.com>
2016-06-10 18:50:18 +0530

    Customized Animal List View

--- app/src/main/java/com/example/suryasolanki/dogbazaar/AnimalsList.java
+++ app/src/main/java/com/example/suryasolanki/dogbazaar/AnimalsList.java
@@ -4,13 +4,50 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class AnimalsList extends AppCompatActivity {
 
+    ListView animalList;
+    int ImageNext=R.mipmap.arrow;
+    String[] AnimalName={
+            "Afghan Shepherd",
+            "Akita Inu",
+            "Alaskan Klee Kai",
+            "American Bulldog",
+            "Boxer",
+            "Bulldog",
+            "Alaskan Klee Kai"
+    };
+    int Price[]={
+            1000,
+            2342,
+            5345,
+            8798,
+            4567,
+            5687,
+            98754
+    };
+
+    int[] ImageId={
+            R.mipmap.akita,
+            R.mipmap.boxer,
+            R.mipmap.braque,
+            R.mipmap.kukai,
+            R.mipmap.racibroz,
+            R.mipmap.shepherd,
+            R.mipmap.kukai,
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animals_list);
+        AnimalListAdapter animalListAdapter=new AnimalListAdapter(this,ImageId,AnimalName,Price,ImageNext);
+        animalList=(ListView)findViewById(R.id.listAnimals);
+        animalList.setAdapter(animalListAdapter);
+
+
     }
 
     @Override
